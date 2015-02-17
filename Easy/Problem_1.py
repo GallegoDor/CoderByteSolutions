01b487f80f9c20f9b667e8c46a1e037c2b49beff
Dor Gallego <dor.gallego@gmail.com>
2015-02-16 23:11:45 -0500

    Create Problem_1.py

---
+++ Easy/Problem_1.py
@@ -0,0 +1,10 @@
+#Task 1 - The instructions that were given are below
+#Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
+
+def FirstReverse(str): 
+  # code goes here
+  return str[::-1]
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print FirstReverse(raw_input())
